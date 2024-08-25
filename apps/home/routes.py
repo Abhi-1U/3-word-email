4adbb7ec51ea452eec964f32ebdb2a9ab60d88e8
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2024-08-25 18:23:50 +0530

    initial templates

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,5 +1,6 @@
 
-from flask import request, redirect, url_for, session, jsonify
+from jinja2 import TemplateNotFound
+from flask import render_template, request, redirect, url_for, session, jsonify
 from nylas import Client
 from datetime import datetime, timedelta
 from flask_session.__init__ import Session
@@ -26,3 +27,20 @@ def recent_emails():
     return ai_out['result']['response']
   except Exception as e:
     return f'{e}'  
+
+
+@blueprint.route('/')
+def index():
+  query_params = {"limit": 5}
+  try:
+    messages, _, _ = nylas.messages.list(session["grant_id"], query_params)
+    keywords = list()
+    for message in messages:
+      inputs = [{ "role": "system", "content": "You are a friendly assistant that helps sort emails" },
+    { "role": "user", "content": f"Describe the email contents in 3 words {message.body}, respond with the 3 words only"}]
+      keywords.append(run_ai_llm(inputs))
+    return render_template('home/index.html', segment='index', emails=keywords)
+  except Exception as e:
+    return f'{e}'  
+    
+
